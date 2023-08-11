58a165137f2f63e9719edcd8324e0ea7eb2dbbc5
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-08-11 11:59:52 -0300

    requisito 2 - adicionando input de filtragem de planetas

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,49 +1,70 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { PlanetsListContext } from '../Context/PlanetContext';
+import { PlanetType } from '../types';
 
 function Table() {
   const { planetsInfo } = useContext(PlanetsListContext);
+
+  const [filteredPlanets, setFilteredPlanets] = useState<PlanetType[]>(planetsInfo);
+
+  function filterPlanets(value: string) {
+    const filteredPlanetsInfo = planetsInfo.filter((planet) => planet
+      .name.toLowerCase().includes(value.toLowerCase()));
+    setFilteredPlanets(filteredPlanetsInfo);
+  }
+
+  useEffect(() => {
+    setFilteredPlanets(planetsInfo);
+  }, [planetsInfo]);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {planetsInfo.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
+    <div>
+      <input
+        data-testid="name-filter"
+        type="text"
+        onChange={ (e) => filterPlanets(e.target.value) }
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {filteredPlanets.map((planet) => (
+            <tr key={ planet.name }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
 
-      </tbody>
+        </tbody>
 
-    </table>
+      </table>
+    </div>
   );
 }
 export default Table;
